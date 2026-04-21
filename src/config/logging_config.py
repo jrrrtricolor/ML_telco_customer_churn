46170e92cf01e4b0239f7b82452c60653500eb6d
Júlia Otranto Aulicino <julia.otranto@outlook.com>
2026-04-21 03:21:06 -0300

    Add prometheus and logs

--- src/config/logging_config.py
+++ src/config/logging_config.py
@@ -1,5 +1,28 @@
 import logging
+import sys
+from datetime import datetime
+from pythonjsonlogger import jsonlogger
 
+class JsonFormatter(jsonlogger.JsonFormatter):
+    def add_fields(self, log_record, record, message_dict):
+        super().add_fields(log_record, record, message_dict)
+        log_record['timestamp'] = datetime.utcnow().isoformat() + 'Z'
+        log_record['level'] = record.levelname
+        log_record['logger'] = record.name
+        log_record['services'] = 'churn_prediction_api'
+
+        if not log_record.get('message'):
+            log_record['message'] = record.getMessage()
+
+def setup_api_logger():
+    logger = logging.getLogger("churn_prediction_api")
+    logger.setLevel(logging.INFO)
+    handler = logging.StreamHandler(sys.stdout)
+    formatter = JsonFormatter()
+    handler.setFormatter(formatter)
+    logger.handlers = []
+    logger.addHandler(handler)
+    return logger
 
 def setup_logging():
     logging.basicConfig(
---
+++ src/metrics.py
@@ -0,0 +1,6 @@
+from prometheus_client import Counter, Gauge, Histogram, Summary
+
+PREDICTIONS_TOTAL = Counter('predictions_total', 'Total number of predictions made')
+ERRORS_TOTAL = Counter('api_errors_total', 'Total number of errors encountered on api')
+PREDICTION_DURATION = Histogram('prediction_duration_seconds', 'Duration of prediction processing in seconds')
+AVG_CONFIDENCE = Histogram('average_prediction_confidence', 'Average confidence of predictions')
